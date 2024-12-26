23607299c545cc04e07da139e3aee3e71f51b6f5
aripp666 <ilham22ti@mahasiswa.pcr.ac.id>
2024-12-26 19:11:17 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import './footer.css';
+import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
         <p>&copy; 2024 Apotek Spotter. All rights reserved.</p>
-        <div className="social-links">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
-        </div>
       </div>
     </footer>
   );
